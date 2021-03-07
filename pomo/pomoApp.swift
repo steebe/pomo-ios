70b69c84560d92792c8b6d1a98a06becde061d3c
steebe <steve@stevebass.me>
2021-03-06 19:53:01 -0800

    Initial Commit

---
+++ pomo/pomoApp.swift
@@ -0,0 +1,17 @@
+//
+//  pomoApp.swift
+//  pomo
+//
+//  Created by Steve Bass on 3/6/21.
+//
+
+import SwiftUI
+
+@main
+struct pomoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
